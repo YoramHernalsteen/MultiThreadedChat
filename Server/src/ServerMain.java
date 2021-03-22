0ad84d9010595dd56dbaf0ad7122d598f9068fe6
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-03-22 23:20:25 +0100

    using 2 threads for 2 processes

--- Server/src/ServerMain.java
+++ Server/src/ServerMain.java
@@ -3,6 +3,8 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*Main thread will handle accept, client connections will be handled by other thread*/
+
 public class ServerMain {
     public static void main(String[] args) {
         int port = 8088;
@@ -10,15 +12,36 @@ public class ServerMain {
             ServerSocket serverSocket = new ServerSocket(port);
             while(true){
                 Socket clientSocket = serverSocket.accept();
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello world \n".getBytes());
-                clientSocket.close();
+                Thread threadToHandleConnections = new Thread(){
+                    @Override
+                    public void run(){
+                        try {
+                            handleConnection(clientSocket);
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                };
+                threadToHandleConnections.start();
+
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace();{
+
+            }
         }
 
 
+    }
+
+    private static void handleConnection(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i =0 ; i<10; i++){
+            outputStream.write(("Helloo" + i + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
 }
